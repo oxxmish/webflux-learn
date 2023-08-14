ee5f93c528fa61b2b7bbae14cd4c98d28e21c6c6
Чеботаряну Михаил <misha18031999@gmail.com>
2023-08-14 10:46:45 +0300

    init

---
+++ src/main/kotlin/com/oxxmish/webfluxlearn/WebfluxLearnApplication.kt
@@ -0,0 +1,11 @@
+package com.oxxmish.webfluxlearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxLearnApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebfluxLearnApplication>(*args)
+}
---
+++ src/test/kotlin/com/oxxmish/webfluxlearn/WebfluxLearnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oxxmish.webfluxlearn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxLearnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
